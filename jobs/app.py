bd0f1fde5f356b8966d23aa9d89328c5dc1be2b2
iasmami <ines.asmami@capgemini.com>
2023-03-16 10:20:26 +0100

    Completed module

---
+++ jobs/app.py
@@ -0,0 +1,33 @@
+from flask import (Flask, render_template)
+
+# import sqlite3
+
+# PATH = "db/jobs.sqlite"
+
+app = Flask(__name__)
+
+# def open_connection():
+#     db = getattr(g, '_database', None)
+#     if db is None:
+#         db.row_factory = sqlite3.Row
+#         db = g._database = sqlite3.connect(PATH)
+#     return db
+
+# def execute_sql(sql, values, commit, single):
+#     values = ()
+#     commit = False
+#     single = False
+#     connection = open_connection()
+#     connection.execute(sql, values)
+#     if commit == True:
+#         results = connection.commit()
+#     else:
+#         results = cursor.fetchone() 
+#         if single else cursor.fetchall()
+
+@app.route('/')
+@app.route('/jobs')
+def jobs():
+     return render_template(
+         "index.html"
+    )
